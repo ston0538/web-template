cf7ac59ca39a0f932ef09ca4062618cb326c4518
KHK <ston0538@gmail.com>
2021-03-28 04:43:03 +0900

    feat: svelte-kit을 설치하고 layout을 설정하였습니다.

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts"></script>
+
+<nav class="nav">
+	<a href="/">Home</a>
+	<a href="contact">Contact</a>
+</nav>
+
+<slot />
+
+<style>
+	.nav {
+		border: 1px solid black;
+	}
+</style>
